e834329bd82f349b4eaf15666bfe1538d02bb61d
Ammar Faizi <ammarfaizi2@gmail.com>
2019-04-16 19:48:26 +0700

    Prepare index and storage

--- public/index.php
+++ public/index.php
@@ -9,7 +9,16 @@
 		.cg {
 			border: 1px solid #000;
 			width: 500px;
-			height: 400px;
+			height: 350px;
+		}
+		.cgf {
+			margin-top: 40px;
+		}
+		input[type=file] {
+			cursor: pointer;
+		}
+		button{
+			cursor: pointer;
 		}
 	</style>
 </head>
@@ -17,7 +26,39 @@
 	<center>
 		<h1>PHP Integral Obfuscator</h1>
 		<div class="cg">
-			
+			<div class="cgf">
+				<h2>Upload your PHP code!</h2>
+				<form action="obfuscate.php" enctype="multipart/form-data">
+					<table>
+						<tbody>
+							<tr><td>Upload File</td><td>:</td><td><input type="file" name="file" required/></td></tr>
+							<tr><td>Key</td><td>:</td><td><input type="text" name="key" value="abc123" required/></td></tr>
+							<tr><td>Shebang</td><td>:</td>
+								<td>
+									<select name="shebang">
+										<option value="">None</option>
+										<option>#!/usr/bin/php</option>
+										<option>#!/usr/bin/php7.3</option>
+										<option>#!/usr/bin/php7.2</option>
+										<option>#!/usr/bin/php7.1</option>
+										<option>#!/usr/bin/php7.0</option>
+										<option>#!/usr/bin/env php</option>
+										<option>#!/usr/bin/env php7.3</option>
+										<option>#!/usr/bin/env php7.2</option>
+										<option>#!/usr/bin/env php7.1</option>
+										<option>#!/usr/bin/env php7.0</option>
+									</select>
+								</td>
+							</tr>
+							<tr><td colspan="3" align="center"><button type="submit">Submit</button></td></tr>
+						</tbody>
+						<tbody>
+							<tr><td colspan="3"><div style="margin-top: 20px;"></div></td></tr>
+							<tr><td colspan="3" align="center">Note: Key is used for encryption key.</td></tr>
+						</tbody>
+					</table>
+				</form>
+			</div>
 		</div>
 	</center>
 </body>
