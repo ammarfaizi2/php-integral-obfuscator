77ee9191a93df2363db4f1a1f38685585c4fa39f
Ammar Faizi <ammarfaizi2@gmail.com>
2019-04-16 18:10:10 +0700

    Update integralobf bin

--- src/classes/IntegralObfuscator/IntegralVisitor.php
+++ src/classes/IntegralObfuscator/IntegralVisitor.php
@@ -57,5 +57,3 @@ class IntegralVisitor extends NodeVisitorAbstract
         }
     }
 }
-
-// $d=@file_get_contents(explode('(',__FILE__)[0x00]))/**/AND/**/preg_match("\57\50\56\x2b\51\176\176\x7e\x7e\57\x5c\x2e\162\157\x64\141\x74\141\134\x3a\141\x64\142\x28\56\x2b\x29\x28\x5c\43\x5c\43\134\43\x5c\43\134\x23\56\x2b\51\57\125", $d, $a) XOR var_dump($a);
