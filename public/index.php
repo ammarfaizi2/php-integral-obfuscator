536feba16a82703a85bb4d21080233849b56a58b
Ammar Faizi <ammarfaizi2@gmail.com>
2019-04-16 19:36:59 +0700

    Prepare web version

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Integral Obfuscator</title>
+	<style type="text/css">
+		* {
+			font-family:Arial;
+		}
+		.cg {
+			border: 1px solid #000;
+			width: 500px;
+			height: 400px;
+		}
+	</style>
+</head>
+<body>
+	<center>
+		<h1>PHP Integral Obfuscator</h1>
+		<div class="cg">
+			
+		</div>
+	</center>
+</body>
+</html>
